dd4a0e3cb3d4fd923d2ed7e7118c31d9e5ae08f5
Aurélie Gilet <aur.gilet@gmail.com>
2021-04-21 10:11:11 +0200

    integrating local bootstrap and jquery lib

--- site_immobilier/include/header.inc.php
+++ site_immobilier/include/header.inc.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="../CDN/bootstrap_4.6/css/bootstrap.min.css"> 
 
         <style>
             body {background-color: #eae1f2;}
